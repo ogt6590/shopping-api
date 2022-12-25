ea7e68b93000155f69db8d64214620b71dff5bd7
DESKTOP-7AASSM0\오경택 <ogt6590@gmail.com>
2022-12-25 21:34:27 +0900

    shopping-web 프로젝트 생성

---
+++ src/main/java/com/shopping/shoppingweb/ShoppingWebApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.shoppingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopping/shoppingweb/ShoppingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.shoppingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
